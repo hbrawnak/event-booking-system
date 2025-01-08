6f4d5ce8475f8d114d5c68ddb78b8367e03d040c
Habibur Rahman <hbrawnak@gmail.com>
2025-01-08 19:56:06 +0600

    fixed query filter and security

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-require_once __DIR__ . '/controllers/BookingController.php';
+require_once __DIR__ . '/controllers/ParticipantsController.php';
 
-$controller = new BookingController();
+$controller = new ParticipantsController();
 
 $filters = [
     'employee_name' => isset($_GET['employee_name']) ? $_GET['employee_name'] : '',
@@ -9,7 +9,12 @@ $filters = [
     'event_date' => isset($_GET['event_date']) ? $_GET['event_date'] : '',
 ];
 
-$result = $controller->getData($filters);
+$page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+$page = max($page, 1);
+$limit = isset($_GET['limit']) ? (int) $_GET['limit'] : 10;
+$offset = ($page - 1) * $limit;
+
+$result = $controller->getData($filters, $limit, $offset);
 
 $fees = array_map(function ($item) {
     return $item['participation_fee'];
@@ -18,4 +23,4 @@ $fees = array_map(function ($item) {
 $totalPrice = array_sum($fees);
 
 
-include __DIR__ . '/views/booking.php';
+include __DIR__ . '/views/participants.php';
