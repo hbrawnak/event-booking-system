64b2da6eb8dfbada4be2e82922e6c21df1fa34b5
Habibur Rahman <hbrawnak@gmail.com>
2025-02-26 00:11:41 +0600

    updated version comparator

--- utils/VersionComparator.php
+++ utils/VersionComparator.php
@@ -1,7 +1,33 @@
 <?php
 
+namespace App\utils;
+
 class VersionComparator {
-    public static function isOldVersion($version) {
+    /*public static function isOldVersion($version) {
         return version_compare($version, '1.0.17+60', '<=');
+    }*/
+
+    public static function isOldVersion($version) {
+        $currentVersion = '1.0.17+60';
+
+        $versionParts = self::splitVersion($version);
+        $currentParts = self::splitVersion($currentVersion);
+
+        foreach ($versionParts as $index => $part) {
+            if (!isset($currentParts[$index])) {
+                return false;
+            }
+            if ((int) $part < (int) $currentParts[$index]) {
+                return true;
+            } elseif ((int) $part > (int) $currentParts[$index]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static function splitVersion($version) {
+        return preg_split('/[.+]/', $version);
     }
 }
